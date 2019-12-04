11e0ac9547a75f9daf7ccf4b9217d14ba0cc9b44
HarryChen0506 <chenwenhua0506@sina.com>
2019-12-04 18:13:33 +0800

    feat: line

--- src/Group.js
+++ src/Group.js
@@ -1,6 +1,10 @@
 export class Group {
   constructor() {
     this.children = []
+    this.rotation = 0
+    this.translation = {x: 0, y: 0}
+    this.scale = 1
+    this.matrix = []
   }
   add(object) {
     if (object) {
--- src/shapes/Line.js
+++ src/shapes/Line.js
@@ -1,4 +1,13 @@
 export class Line {
+  constructor(options = {}) {
+    const { x1, y1, x2, y2 } = options
+    this.rotation = 0
+    this.translation = { x: 0, y: 0 }
+    this.scale = 1
+    this.matrix = []
+    this.color = '#000'
+    this.lineWidth = 1
+  }
   draw() {
     console.log('line')
   }

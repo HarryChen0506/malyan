8cfea6b9c4f2db601bc0108531bba6c26443a4a2
HarryChen0506 <chenwenhua0506@sina.com>
2020-01-09 22:05:57 +0800

    perf: examples demo

---
+++ examples/js/base.js
@@ -0,0 +1,14 @@
+(function (window) {
+
+  document.write('<link rel="stylesheet" type="text/css" href="https://cdn.bootcss.com/codemirror/5.48.4/codemirror.css">')
+  document.write('<link rel="stylesheet" type="text/css" href="https://cdn.bootcss.com/codemirror/5.48.4/theme/3024-night.css">')
+  document.write('<script src="https://cdn.bootcss.com/codemirror/5.48.4/codemirror.js"><\/script>')
+  document.write('<script src="https://cdn.bootcss.com/codemirror/5.48.4/mode/javascript/javascript.min.js"><\/script>')
+
+  document.write('<link rel="stylesheet" type="text/css" href="./styles/base.css">')
+  document.write('<link rel="stylesheet" type="text/css" href="./styles/demo.css">')
+  document.write('<script src="./js/code.js"><\/script>')
+
+  window.Malyan || document.write('<script src="https://unpkg.com/malyan@0.0.2/dist/malyan.min.js"><\/script>')
+
+})(this)

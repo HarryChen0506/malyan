e9cc48abb4676a1442b7c13b11527bab91040708
HarryChen0506 <chenwenhua0506@sina.com>
2020-02-01 19:46:30 +0800

    fix: test2

--- karma.conf.js
+++ karma.conf.js
@@ -137,14 +137,14 @@ module.exports = function (config) {
 
   if (process.env.TRAVIS) {
     console.log('process.env.TRAVIS', process.env.TRAVIS)
-    configuration.browsers = ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox']
+    // configuration.browsers = ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox']
     // you can define custom flags
-    configuration.customLaunchers = {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    }
+    // configuration.customLaunchers = {
+    //   ChromeHeadlessNoSandbox: {
+    //     base: 'ChromeHeadless',
+    //     flags: ['--no-sandbox']
+    //   }
+    // }
   }
 
   config.set(configuration)

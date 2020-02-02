476407886a97eebae49cdc82fb05043116524099
HarryChen0506 <chenwenhua0506@sina.com>
2020-02-02 22:19:45 +0800

    test: cover2

--- karma.conf.js
+++ karma.conf.js
@@ -96,10 +96,10 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress',  'coverage-istanbul', 'coveralls'],
+    reporters: ['progress', 'coverage-istanbul', 'coveralls'],
 
     coverageIstanbulReporter: {
-      reports: ['html', 'text-summary'],
+      reports: ['html', 'lcov', 'text-summary'],
       dir: 'coverage/',
       fixWebpackSourcePaths: true,
       skipFilesWithNoCoverage: true,

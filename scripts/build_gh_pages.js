0916f1f2aeb2544b6fd0c4e6ceeb6d64f9c23020
HarryChen0506 <chenwenhua0506@sina.com>
2020-02-04 17:29:54 +0800

    docs: home

--- scripts/build_gh_pages.js
+++ scripts/build_gh_pages.js
@@ -1,5 +1,7 @@
 const { buildDocs } = require('./build_docs')
 const { buildExamples } = require('./build_examples')
+const { buildHome } = require('./build_home')
 
 buildDocs()
 buildExamples()
+buildHome()
